0a98dcd1af5a307e80044baeecb5ac10a7af63fe
Jaewon <20011211s@gmail.com>
2024-04-06 13:44:50 +0900

    first Commit

---
+++ src/main/java/Project/quantHelper/QuantHelperApplication.java
@@ -0,0 +1,13 @@
+package Project.quantHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Project/quantHelper/QuantHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package Project.quantHelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
