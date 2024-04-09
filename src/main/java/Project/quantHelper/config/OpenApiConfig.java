22caf551941718e12d1377d6ee9012ad28c2ecd7
sehwan505 <sehwan505@gmail.com>
2024-04-09 20:29:59 +0900

    Feat: OpenApi config 추가

---
+++ src/main/java/Project/quantHelper/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package Project.quantHelper.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("stock data plugin")
+                .description("stock data plugin")
+                .version("0.1.0");
+    }
+}
