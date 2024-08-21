e17271296b9a2f05dcbce8d018c1269548a0c033
Jaewon <20011211s@gmail.com>
2024-08-21 22:46:00 +0900

    Feat: 주식 총 발행 수, EPS 등을 Dart로 가져오는 API 생성

--- src/main/java/Project/quantHelper/controller/FinancialStatementController.java
+++ src/main/java/Project/quantHelper/controller/FinancialStatementController.java
@@ -10,6 +10,10 @@ import Project.quantHelper.service.FinancialStatementService;
 import Project.quantHelper.service.StockService;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -17,6 +21,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -29,6 +34,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/financial-statement")
 @RequiredArgsConstructor
+@Slf4j
 public class FinancialStatementController {
 
     private final DartService dartService;
@@ -73,22 +79,77 @@ public class FinancialStatementController {
         if (request.getQuarter() <= 0){
             return ResponseEntity.badRequest().body("quater should be upper zero");
         }
-        String financialStatementContent = "";
+        String content = null;
+        String majorContent = "";
+        JsonParser jsonParser = new JsonParser();
         StockDTO stockDTO = stockService.getStockDTOByStockName(request.getCorpName());
         try {
             FinancialStatementDTO financialStatementDTO = financialStatementService.findFinancialStatementByStockId(stockDTO.getStockID(), request.getYear(), request.getQuarter());
-            financialStatementContent = financialStatementDTO.getContent();
+            majorContent = financialStatementDTO.getContent();
         } catch (RuntimeException e) {
-            Mono<String> financialStatementResponse = dartService.getFinancialStatementFromDart(stockDTO, request.getYear(), request.getQuarter());
-            financialStatementContent = financialStatementResponse.block();
+            // Getting major content from Dart
+            majorContent = dartService.getFinancialStatementFromDart(stockDTO, request.getYear(), request.getQuarter()).block();
+
+            // Parsing first JSON
+            JsonObject jsonObject = jsonParser.parse(dartService.getStockAmountFromDart(stockDTO, request.getYear(), request.getQuarter()).block()).getAsJsonObject();
+            JsonArray list = jsonObject.get("list").getAsJsonArray();
+
+            // Creating extraContent object for second JSON
+            JsonObject extraContent = new JsonObject();
+
+            for (JsonElement element : list) {
+                JsonObject obj = element.getAsJsonObject();
+
+                // Extracting specific fields
+                if (obj.get("se").getAsString().equals("합계")) {
+                    extraContent.addProperty("발행 주식의 총 수", obj.get("istc_totqy").getAsString());
+                    break;
+                }
+            }
+
+            JsonObject alloJsonObject = jsonParser.parse(dartService.getAllocationAndEpsFromDart(stockDTO, request.getYear(), request.getQuarter()).block()).getAsJsonObject();
+            JsonArray alloList = alloJsonObject.get("list").getAsJsonArray();
+            for (JsonElement element : alloList) {
+                JsonObject obj = element.getAsJsonObject();
+
+                if (obj.get("se").getAsString().equals("(연결)주당순이익(원)")) {
+                    extraContent.addProperty("EPS", obj.get("thstrm").getAsString());
+                }
+
+                if (obj.get("se").getAsString().equals("주당 현금배당금(원)")) {
+                    if (obj.get("stock_knd").getAsString().equals("보통주")) {
+                        extraContent.addProperty("보통주 주당 배당금", obj.get("thstrm").getAsString());
+                    } else if (obj.get("stock_knd").getAsString().equals("우선주")) {
+                        extraContent.addProperty("우선주 주당 배당금", obj.get("thstrm").getAsString());
+                    }
+                }
+            }
+
+            // Merging majorContent and extraContent into one JSON object
+            JsonObject mergedContent = new JsonObject();
+            JsonObject majorContentJson = jsonParser.parse(majorContent).getAsJsonObject();
+
+            // Adding major content to merged JSON
+            for (Map.Entry<String, JsonElement> entry : majorContentJson.entrySet()) {
+                mergedContent.add(entry.getKey(), entry.getValue());
+            }
+
+            // Adding extra content to merged JSON
+            for (Map.Entry<String, JsonElement> entry : extraContent.entrySet()) {
+                mergedContent.add(entry.getKey(), entry.getValue());
+            }
+
+            content = mergedContent.toString();
+
+            // Save merged content
             FinancialStatementDTO financialStatementDTO = FinancialStatementDTO.builder()
                     .stockId(stockDTO.getStockID())
                     .year(request.getYear())
                     .quarter(request.getQuarter())
-                    .content(financialStatementContent)
+                    .content(content)
                     .build();
             financialStatementService.save(financialStatementDTO);
         }
-        return ResponseEntity.ok().body(financialStatementContent);
+        return ResponseEntity.ok().body(content);
     }
 }
--- src/main/java/Project/quantHelper/service/DartService.java
+++ src/main/java/Project/quantHelper/service/DartService.java
@@ -51,4 +51,44 @@ public class DartService {
                 .retrieve()
                 .bodyToMono(String.class);
     }
+
+    /**
+     * 발행된 주식의 총 수를 Dart open api로 불러오는 메소드
+     * @param corpName, year, quarter
+     * @return String
+     */
+    public Mono<String> getStockAmountFromDart(StockDTO stockDTO, int year, int quarter) {
+        String[] reprtCode = {"11013", "11012", "11014", "11011"};
+
+        String fullUrl = baseUrl + "/api/stockTotqySttus.json" +
+                "?crtfc_key=" + apiKey +
+                "&corp_code=" + stockDTO.getCorpCode() +
+                "&bsns_year=" + year +
+                "&reprt_code=" + reprtCode[quarter - 1];
+
+        return webClient.get()
+                .uri(fullUrl)
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    /**
+     * 주당 배당금과 주당순이익(EPS)를 Dart open api로 불러오는 메소드
+     * @param corpName, year, quarter
+     * @return String
+     */
+    public Mono<String> getAllocationAndEpsFromDart(StockDTO stockDTO, int year, int quarter) {
+        String[] reprtCode = {"11013", "11012", "11014", "11011"};
+
+        String fullUrl = baseUrl + "/api/alotMatter.json" +
+                "?crtfc_key=" + apiKey +
+                "&corp_code=" + stockDTO.getCorpCode() +
+                "&bsns_year=" + year +
+                "&reprt_code=" + reprtCode[quarter - 1];
+
+        return webClient.get()
+                .uri(fullUrl)
+                .retrieve()
+                .bodyToMono(String.class);
+    }
 }
